26e1b3553d821e07023d1fb5c85c18c0114e20d2
TCG <ssibonokuhle@gmail.com>
2021-02-03 15:34:13 +0200

    added  the code for the main component

--- src/Components/SearchBar/Search.js
+++ src/Components/SearchBar/Search.js
@@ -1,29 +1,78 @@
-class App extends React.Components {
-    state = {
-        names: [
-            'Kuhle',
-            'John',
-            'Leonard',
-            'Bob',
-            'Queen',
-        ],
-        searchTerm: ""
-    }
-editSearchTerm = (e) => {
-    this.setState({editSearchTerm: e.target.value})
-}
-dynamicSearch = () => {
-    return this.names.filter(name => name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
-}
-render() {
-    return (
+// import React, { Component } from 'react'
 
-<div style = {{textAlign: 'center', paddingTop: '30vh'}}>
-<input type= "text" value= {this.state.searchTerm} onChange={this.editSearchTerm} placeholder= 'Search for a name!'/>
-   <br></br>
-<h3>These are the important names:</h3>   
-<NamesContainer name = {this.dynamicSearch()}/>  
-</div>
-);
+
+// class App extends React.Components {
+//     state = {
+//         names: [
+//             'Kuhle',
+//             'John',
+//             'Leonard',
+//             'Bob',
+//             'Queen',
+//         ],
+//         searchTerm: ""
+//     }
+// editSearchTerm = (e) => {
+//     this.setState({editSearchTerm: e.target.value})
+// }
+// dynamicSearch = () => {
+//     return this.names.filter(name => name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+// }
+// render() {
+//     return (
+
+// <div style = {{textAlign: 'center', paddingTop: '30vh'}}>
+// <input type= "text" value= {this.state.searchTerm} onChange={this.editSearchTerm} placeholder= 'Search for a name!'/>
+//    <br></br>
+// <h3>These are the important names:</h3>   
+// <NamesContainer name = {this.dynamicSearch()}/>  
+// </div>
+// );
+//     }
+// }
+
+
+import React from 'react';
+
+// export default class SearchBarComponent extends React.Component {
+
+class Form extends Component {
+    state = {
+        initialItems: [],
+        items: []
     }
-}
+
+    filterList = (event) => {
+      let items = this.state.initialItems;
+      items = items.filter((item) => {
+        return item.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+      });
+      this.setState({items: items});
+    }
+
+    componentWillMount = () => {
+      this.setState({
+          initialItems: this.props.content,
+          items: this.props.content
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          <form>
+                <input type="text" placeholder="Search" onChange={this.filterList}/>
+          </form>
+          <div>
+            {
+                this.state.items.map(function(item) {
+                    return <div key={item}>{item}</div>
+                })
+
+                
+            }
+            </div>
+        </div>
+      );
+    }
+};
